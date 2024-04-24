5d695094448b85f38bb6e03fe6cb1705c52c0e2a
NoahSaas <151681167+NoahSaas@users.noreply.github.com>
2024-04-24 13:47:31 +0200

    .

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, sys, random, time
+import pygame, sys, random, time, math
 
 def reset_ball():
     global ball_speed_x, ball_speed_y
@@ -20,41 +20,7 @@ def point_won(winner):
 
 
 def animate_ball():
-    global ball_speed_x, ball_speed_y
-    ball.x += ball_speed_x
-    ball.y += ball_speed_y
-
-    # Check collision with walls
-    if ball.bottom >= screen_height or ball.top <= 0:
-        ball_speed_y *= -1
-
-    # Check collision with player1's dash racket
-    if player1.dashing:
-        if ball.colliderect(player1):
-            ball_speed_x *= -1.05
-            ball_speed_y += (ball.centery - player1.rect.centery) * 0.05  # Transfer momentum based on collision position
-            reset_ball()
-            return 
-
-    if player2.dashing:
-        if ball.colliderect(player2):
-            ball_speed_x *= -1.05
-            ball_speed_y += (ball.centery - player2.rect.centery) * 0.05  # Transfer momentum based on collision position
-            reset_ball()
-            return
-
-    if ball.colliderect(player1):
-        ball_speed_x *= -1.05
-        ball_speed_y += (ball.centery - player1.rect.centery) * 0.05  # Transfer momentum based on collision position
-    if ball.colliderect(player2):
-        ball_speed_x *= -1.05
-        ball_speed_y += (ball.centery - player2.rect.centery) * 0.05  # Transfer momentum based on collision position
-
-    if ball.right >= screen_width:
-        point_won("player1")
-    if ball.left <= 0:
-        point_won("player2")
-
+    pass
 
 def animate_player1():
     player1.rect.y += player1.y_speed * player1.speed_multiplier
@@ -73,6 +39,7 @@ def animate_player1():
 
     if player1.rect.left <= 0:
         player1.rect.left = 0
+
 
 def animate_player2():
     player2.rect.y += player2.y_speed * player2.speed_multiplier
@@ -95,17 +62,39 @@ def animate_player2():
 
 def dash(player):
     if not player.dashing and time.time() - player.last_dash_time >= 1:
+        direction = (player.x_speed, player.y_speed)
+        if direction == (0, 0):
+            direction = (1, 0)
+        
+        length = math.sqrt(direction[0] ** 2 + direction[1] ** 2)
+        direction = (direction[0] / length, direction[1] / length)
+        
+        print("Direction:", direction)
+        
         player.speed_multiplier = 2
         player.dashing = True
+        player.dash_direction = direction
         player.dash_start_time = time.time()
         player.last_dash_time = time.time()
-        player.dash_start_pos = player.rect.center  # Store the starting position of the dash
+        player.dash_start_pos = player.rect.center
 
 
 def animate_dash_racket(player):
     if player.dashing:
-        racket = Racket(player.dash_start_pos[0], player.dash_start_pos[1], 30, 100)
-        pygame.draw.rect(screen, 'pink', racket)  # Draw the dash rectangle
+        dash_line_points = []
+
+        print("Dash direction:", player.dash_direction)
+
+        step_x = player.dash_direction[0]
+        step_y = player.dash_direction[1]
+
+        for i in range(100):
+            dash_line_points.append((int(player.dash_start_pos[0] + step_x * i), int(player.dash_start_pos[1] + step_y * i)))
+
+        for point in dash_line_points:
+            pygame.draw.circle(screen, 'pink', point, 2)
+
+        return dash_line_points
 
 
 def handle_movement(plr):
@@ -125,10 +114,19 @@ def handle_movement(plr):
         if keys[pygame.K_SPACE]:
             dash(plr)
 
+        if plr.dashing:
+            plr.x_speed = plr.dash_direction[0] * 5
+            plr.y_speed = plr.dash_direction[1] * 5
+
         if plr.dashing and time.time() - plr.dash_start_time >= 0.15:
             plr.speed_multiplier = 1
             plr.dashing = False  
 
+        for racket in rackets:
+            for point in racket:
+                if plr.rect.colliderect(pygame.Rect(point[0], point[1], 2, 2)):  # Adjust the collision rectangle based on your line representation
+                    print("Collision")
+            
 
     if plr == player2:
         if keys[pygame.K_UP] and not keys[pygame.K_DOWN]:
@@ -141,9 +139,18 @@ def handle_movement(plr):
         elif keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT]:
             plr.x_speed = 5
 
+        if plr.dashing:
+            plr.x_speed = plr.dash_direction[0] * 5
+            plr.y_speed = plr.dash_direction[1] * 5
+
         if plr.dashing and time.time() - plr.dash_start_time >= 1.5:
             plr.speed_multiplier = 1
             plr.dashing = False 
+
+        for racket in rackets:
+            for point in racket:
+                if plr.rect.colliderect(pygame.Rect(point[0], point[1], 2, 2)):  # Adjust the collision rectangle based on your line representation
+                    print("Collision")
 
 
 class Player():
@@ -157,12 +164,6 @@ class Player():
         self.dash_start_time = 0  
         self.last_dash_time = 0
         self.dash_start_pos = 0
-
-
-class Racket():
-    def __init__(self, x, y, width, height):
-        self.rect = pygame.Rect(x, y, width, height)
-
 
 
 pygame.init()
@@ -180,7 +181,7 @@ ball.center = (screen_width/2, screen_height/2)
 
 player1 = Player(150, screen_height/2, 20)
 player2 = Player(screen_width - 150, screen_height/2, 20)
-
+rackets = []
 
 ball_speed_x = 0
 ball_speed_y = 0
@@ -195,16 +196,20 @@ while True:
             sys.exit()
     
     
+    screen.fill('black')
+    
+    
     handle_movement(player1)
     handle_movement(player2)
     animate_ball()
     animate_player1()
     animate_player2()
-    animate_dash_racket(player1)
-    animate_dash_racket(player2)
-        
-    screen.fill('black')
 
+    if player1.dashing:
+        rackets.append(animate_dash_racket(player1))
+    if player2.dashing:
+        rackets.append(animate_dash_racket(player2))
+        
     plr1_score_surface = score_font.render(str(player1.points), True, "pink")
     plr2_score_surface = score_font.render(str(player2.points), True, "pink")
     screen.blit(plr1_score_surface,(screen_width/4,20))
@@ -214,6 +219,10 @@ while True:
     pygame.draw.ellipse(screen,'pink',ball)
     pygame.draw.rect(screen,'pink',player1)
     pygame.draw.rect(screen,'pink',player2)
+    
+    for line_points in rackets:
+        for point in line_points:
+            pygame.draw.circle(screen, 'pink', point, 2)
 
     pygame.display.update()
     clock.tick(60)
