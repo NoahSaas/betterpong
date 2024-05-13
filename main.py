8e969de30947957b58c47fe67a9707273bbf8723
NoahSaas <151681167+NoahSaas@users.noreply.github.com>
2024-05-13 10:28:59 +0200

    .

--- main.py
+++ main.py
@@ -75,6 +75,31 @@ def animate_player1():
         player1.rect.left = 0
 
 
+def draw(players, background):
+    screen.fill(background)
+
+    plr1_score_surface = score_font.render(str(players[0].points), True, "pink")
+    plr2_score_surface = score_font.render(str(players[1].points), True, "pink")
+
+
+    screen.blit(plr1_score_surface,(screen_width/4,20))
+    screen.blit(plr2_score_surface,(3*screen_width/4,20))
+
+    pygame.draw.aaline(screen,'pink',(screen_width/2, 0), (screen_width/2, screen_height))
+    pygame.draw.aaline(screen,'blue',(screen_width/4, 0), (screen_width/4, screen_height))
+    pygame.draw.aaline(screen,'blue',(screen_width - (screen_width/4), 0), (screen_width - (screen_width/4), screen_height))
+    pygame.draw.ellipse(screen,'pink',ball)
+
+    if players[0].dashing:
+        pygame.draw.rect(screen,'green',players[0])
+    else:
+        pygame.draw.rect(screen,'pink',players[0])
+    if players[1].dashing:
+        pygame.draw.rect(screen,'green',players[1])
+    else:
+        pygame.draw.rect(screen,'pink',players[1])
+    
+
 def animate_player2():
     player2.rect.y += player2.y_speed * player2.speed_multiplier
     player2.rect.x += player2.x_speed * player2.speed_multiplier
@@ -105,7 +130,7 @@ def dash(player):
         player.dash_start_pos = player.rect.center
 
 
-def handle_movement(plr):
+def handle_movement(plr, rackets):
     keys = pygame.key.get_pressed()
 
     if plr == player1:
@@ -123,16 +148,12 @@ def handle_movement(plr):
             plr.x_speed = plr.x_speed/math.sqrt(2)
             plr.y_speed = plr.y_speed/math.sqrt(2)
 
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_SPACE] and plr.dashing != True:
             dash(plr)
-
-        if plr.dashing:
-            plr.x_speed = plr.dash_speed_x
-            plr.y_speed = plr.dash_speed_y
 
         if plr.dashing and time.time() - plr.dash_start_time >= 0.25:
             plr.speed_multiplier = 1
-            plr.dashing = False
+            plr.dashing = False  
 
 
 
@@ -151,28 +172,24 @@ def handle_movement(plr):
             plr.x_speed = plr.x_speed/math.sqrt(2)
             plr.y_speed = plr.y_speed/math.sqrt(2)
 
-        if keys[pygame.K_m]:
+        if keys[pygame.K_m] and plr.dashing != True:
             dash(plr)
-
-        if plr.dashing:
-            plr.x_speed = plr.dash_speed_x
-            plr.y_speed = plr.dash_speed_y
 
         if plr.dashing and time.time() - plr.dash_start_time >= 0.25:
             plr.speed_multiplier = 1
             plr.dashing = False  
 
 
-
 class Player():
-    def __init__(self, x, y, size):
-        self.rect = pygame.Rect(x, y, size, size)
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
         self.y_speed = 0
         self.x_speed = 0
         self.vel = 5
         self.speed_multiplier = 1
         self.points = 0
         self.dashing = False
+        self.last_direction = 0
         self.dash_start_time = 0  
         self.last_dash_time = 0
         self.dash_start_pos = 0
@@ -191,11 +208,12 @@ clock = pygame.time.Clock()
 ball = pygame.Rect(0,0,30,30)
 ball.center = (screen_width/2, screen_height/2)
 
-player1 = Player(150, screen_height/2, 20)
-player2 = Player(screen_width - 150, screen_height/2, 20)
+player1 = Player(0, screen_height/2, 10, 150)
+player2 = Player(screen_width - 10, screen_height/2, 10, 150)
 
 ball_speed_x = 0
 ball_speed_y = 0
+rackets = []
 
 score_font = pygame.font.Font(None, 100)
 
@@ -207,33 +225,13 @@ while True:
             sys.exit()
     
     
-    handle_movement(player1)
-    handle_movement(player2)
+    handle_movement(player1, rackets)
+    handle_movement(player2, rackets)
 
     animate_ball()
     animate_player1()
     animate_player2()
         
-    screen.fill('black')
-
-    plr1_score_surface = score_font.render(str(player1.points), True, "pink")
-    plr2_score_surface = score_font.render(str(player2.points), True, "pink")
-
-
-    screen.blit(plr1_score_surface,(screen_width/4,20))
-    screen.blit(plr2_score_surface,(3*screen_width/4,20))
-
-    pygame.draw.aaline(screen,'pink',(screen_width/2, 0), (screen_width/2, screen_height))
-    pygame.draw.ellipse(screen,'pink',ball)
-
-    if player1.dashing:
-        pygame.draw.rect(screen,'green',player1)
-    else:
-        pygame.draw.rect(screen,'pink',player1)
-    if player2.dashing:
-        pygame.draw.rect(screen,'green',player2)
-    else:
-        pygame.draw.rect(screen,'pink',player2)
-
+    draw([player1, player2], "black", rackets)
     pygame.display.update()
     clock.tick(60)
